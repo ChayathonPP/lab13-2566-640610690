59e645c6b07b188d003b74235ba875a7a086b5fc
Chayathon <chayathon_p@cmu.ac.th>
2023-08-08 20:42:37 +0700

    finish

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Chayathon Punpang"
+            studentId="640610690"
           />
         </div>
       </body>
